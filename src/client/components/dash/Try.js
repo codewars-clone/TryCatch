8f1452588648d8dfd2f88946926782e3c134f0b9
Camryn Perry <camrynpaigeperry@gmail.com>
2019-11-23 14:01:29 -0500

    feat: added store and reducer

--- src/client/components/dash/Try.js
+++ src/client/components/dash/Try.js
@@ -3,26 +3,40 @@ import React, { Component } from 'react';
 import axios from 'axios';
 //display photo, name, age... description later
 //how to randomize which user is displayed, based on preferences
+//needs two buttons, like button and x button
+
+//in order to get all users that match preferences, we send a specific request that takes the current users' preferences, and makes a call to the database using those preferences as filters, and specificially if that user has not been "seen" by the current user. Can return all users that match these preferences, and that becomes the stack? or a user is randomly selected from that list.
+//also, the other user's preferences have to match the requested user's info/assets
 export class Try extends Component {
   constructor(){
     super();
     this.state = {
-      user: {}
+      users: [],
+      currentIdx: 0
     };
+    this.handleClick = this.handleClick.bind(this);
   };
   async componentDidMount(){
-    const response = await axios.get(`/server/user/${id}`);
-    const user = response.data;
-    this.setState({user});
+    const response = await axios.get(`/server/users`);
+    const users = response.data;
+    this.setState({users});
+  }
+  handleX(){
+    this.setState({currentIdx:+1})
+    //will make a dispatch to store to handle X
+  }
+  handleLike(){
+    //will make a dispatch to store to handle like
   }
   render() {
     return (
       <div>
-        <img src ={this.state.user.imageUrl} />
-        <h4>{this.state.user.name}</h4>
-        <h4>{this.state.user.age}</h4>
+        <img src ={this.state.user[this.state.currentIdx].imageUrl} />
+        <button onClick= {this.handleLike}></button>
+        <h4>{this.state.user[this.state.currentIdx].name} {this.state.user[this.state.currentIdx].age}</h4>
+        <button onClick= {this.handleX}>X</button>
       </div>
-    )
+    );
   }
 }
 
