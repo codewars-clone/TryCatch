2785ccf7e536ba8457c68c8f78857c8100e63c2a
Camryn Perry <camrynpaigeperry@gmail.com>
2019-11-24 16:51:59 -0500

    feat: worked on getLikes and sendLikes routes, still in development

--- src/server/server.js
+++ src/server/server.js
@@ -4,11 +4,12 @@ const app = express();
 
 const { db } = require('./utils/firebase');
 
-const { getUsers, getSingleUser, getTries } = require('./handlers/users');
+const { getUsers, getSingleUser, getTries, getLikes } = require('./handlers/users');
 
 app.get('/users', getUsers);
 app.get('/users/:userId', getSingleUser);
 app.get('/users/:userId/tries', getTries);
+app.get('/users/:userId/likes', getLikes);
 
 const PORT = 8888;
 app.listen(PORT, () => {
