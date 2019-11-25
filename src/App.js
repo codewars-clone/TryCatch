b6c80762a19faea4d7021edc28b5b9d8082cf31e
Michael Mcmanus <mikemcmanus95@gmail.com>
2019-11-25 11:18:23 -0500

    fix: refactored file tree refactored sign up flow

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { SignUp } from './components';
+import { SignUp } from './client/components/index';
 import { Route } from 'react-router-dom';
+import { Try } from './client/components/index';
 
 function App() {
   return (
     <div className="App">
       <Route path="/signUp" component={SignUp} />
+      <Route path="/try" component={Try} />
     </div>
   );
 }
---
+++ src/client/components/index.js
@@ -0,0 +1,9 @@
+//--------- SIGNUP ------------//
+export { default as SignUp } from './auth/SignUp/SignUp';
+export { default as GeneralInfo } from './auth/SignUp/GeneralInfo';
+export { default as Location } from './auth/SignUp/Location';
+export { default as Preferences } from './auth/SignUp/Preferences';
+export { default as Assets } from './auth/SignUp/Assets';
+export { default as Terms } from './auth/SignUp/Terms';
+//--------- DASHBOARD ------------//
+export { default as Try } from './dash/Try';
