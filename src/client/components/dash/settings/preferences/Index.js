14f69f0ba243310578491c1d290b6bf77e3b6b1e
Sayeed Joseph <sayeedjoseph@Sayeeds-MacBook-Air.local>
2019-11-25 15:09:09 -0500

    test

--- src/client/components/dash/settings/preferences/Index.js
+++ src/client/components/dash/settings/preferences/Index.js
@@ -10,6 +10,7 @@ class Settings extends Component {
         <div className="container">
           <h1 className="title is-1">Settings</h1>
           <hr/>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus in ipsum tempora provident alias vero, optio nihil iure, molestiae accusantium omnis incidunt tenetur officia blanditiis veritatis ea? Eius, necessitatibus ipsa?</p>
         </div>
       </section>
 
