64eb0d73210783dd688b758c090dabf68984773c
Sayeed Joseph <sayeedjoseph@Sayeeds-MacBook-Air.local>
2019-11-25 14:57:24 -0500

    completed full view

--- src/client/components/dash/await/AwaitList.js
+++ src/client/components/dash/await/AwaitList.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 class AwaitList extends Component {
   render() { 
     return ( 
-      <div className='container'>
-        <h1 className="title is-1">Await</h1>
-        <hr />
-      </div>
+      <section className="section">
+        <div className='container'>
+          <h1 className="title is-1">Await</h1>
+          <hr />
+        </div>
+      </section>
+
     );
   }
 }
--- src/client/components/dash/catch/AllCatch.js
+++ src/client/components/dash/catch/AllCatch.js
@@ -3,10 +3,13 @@ class AllCatch extends Component {
 
   render() { 
     return (  
-      <div className="container">
-        <h1 className="title is-1">Catch</h1>
-        <hr/>
-      </div>
+      <section className="section">
+        <div className="container">
+          <h1 className="title is-1">Catch</h1>
+          <hr/>
+        </div>
+      </section>
+
     );
   }
 }
