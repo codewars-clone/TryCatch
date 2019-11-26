d482cb2cb39f534f3a1c9c08a511428a5017a10e
Sayeed Joseph <sayeedjoseph@Sayeeds-MacBook-Air.local>
2019-11-25 20:18:32 -0500

    fix: add name and image to singleCatch

--- src/client/components/dash/catch/SingleCatch.js
+++ src/client/components/dash/catch/SingleCatch.js
@@ -7,11 +7,13 @@ const SingleCatch = () => {
     <div className="media">
       <div className="media-content">
         <figure className="image is-96x96">
-          <img width = '2100px' height='200px' src='' alt=""/>   
+          <img width = '2100px' height='200px' src='https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/fred-jones-whats-new-scooby-doo-96.jpg' alt=""/>   
         </figure>
       </div>
+      <div className="media-content">
+        <h3 className="title is-3">Fred</h3>  
+      </div>
       <div className="media-right">
-        <h4 className="title is-4"> 23 <i class="fas fa-birthday-cake"></i></h4> 
         <Link to='/try'>
           <ChatButton />
         </Link>
