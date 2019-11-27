06af88db92aa204caf060b2ae0fb707c48ece5ea
Marcos Santanna <test_123@TEST-123s-MacBook-Air.local>
2019-11-27 16:30:59 -0500

    working on message reducers

--- src/client/components/chat/ChatRoom.js
+++ src/client/components/chat/ChatRoom.js
@@ -1,102 +1,138 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Messages } from '../index'
-import {connect} from 'react-redux'
-import {getChat} from '../../store/reducers/chat'
-import LoadingScreen from 'react-loading-screen'
-import TryImage from '../auth/try.png'
+import { Messages } from '../index';
+import { connect } from 'react-redux';
+import { getChat } from '../../store/reducers/chat';
+import LoadingScreen from 'react-loading-screen';
+import TryImage from '../auth/try.png';
+import moment from 'moment';
 
 class ChatRoom extends Component {
   constructor(props) {
-    super(props)
-    this.state = {  
-      loadingScreen: true
+    super(props);
+    console.log('name => ', props);
+    this.state = {
+      loadingScreen: true,
+      txt: '',
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  componentDidMount() {
+    let chatId = this.props.match.params.id;
+    const { currChat } = this.props;
+    if (chatId) {
+      setTimeout(() => {
+        this.setState({
+          loadingScreen: false,
+        });
+      }, 2000);
     }
+    this.props.getChat(chatId);
   }
-componentDidMount (){
-  let chatId = this.props.match.params.id
-  const { currChat } = this.props
-  if(chatId && !currChat){
-    setTimeout(() => {
-      this.setState({
-        loadingScreen: false
-      })
-          
-    }, 2000)
+  handleInput(ev) {
+    this.setState({
+      txt: ev.target.value,
+    });
   }
-  this.props.getChat(chatId)
-}
-  render() { 
-    const {currChat} = this.props
-    const { loadingScreen } = this.state
+  handleSubmit(e) {
+    console.log('event => ', e);
+    e.preventDefault();
+    let txt = this.state.txt;
+    let messages = {
+      name: 'Daphyni',
+      time: moment().format('MMMM Do YYYY, h:mm:ss a'),
+      txt,
+    };
+    console.log('TCL: ChatRoom -> handleSubmit -> messages', messages);
+  }
 
-    let image = currChat ? currChat.image : ''
-    let name = currChat ? currChat.name : ''
+  render() {
+    const { currChat } = this.props;
+    const { loadingScreen } = this.state;
 
-    console.log("TCL: ChatRoom -> render -> currChat", currChat)
-    
-    let main = <div className='container'>
-                  <div className="box">
-                    <div className="media">
-                      <div className="media-content">
-                        <figure className="image is-48x48">
-                          <img className='is-rounded' width = '2100px' height='200px' src={image} alt=""/>  
-                        </figure>
-                      </div>
-                      <div className="media-content">
-                        <h3 className="title is-3">{name}</h3>  
-                      </div>
-                      <div className="media-right">
-                        <Link to='/catch'>
-                          <div  className="buttons">
-                            <button className='button is-danger'>
-                              LEAVE CHAT
-                            </button>
-                          </div>
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                  <Messages />
-                  <form id='form'>
-                    <div className="field has-addons">
-                      <div className="control">
-                        <input type="text" name="message" className="input" placeholder="Send message" autoFocus />
-                      </div>
-                      <div className="button is-info">
-                        <button name="submit" className="login-button">Send</button>
-                      </div>
-                    </div>
-                  </form>
+    let image = currChat ? currChat.image : '';
+    let name = currChat ? currChat.name : '';
+    let messages = currChat ? currChat.messages : [];
+
+    console.log('TCL: ChatRoom -> render -> Message', this.state.txt);
+
+    let main = (
+      <div className="container">
+        <div className="box">
+          <div className="media">
+            <div className="media-content">
+              <figure className="image is-48x48">
+                <img
+                  className="is-rounded"
+                  width="2100px"
+                  height="200px"
+                  src={image}
+                  alt=""
+                />
+              </figure>
+            </div>
+            <div className="media-content">
+              <h3 className="title is-3">{name}</h3>
+            </div>
+            <div className="media-right">
+              <Link to="/catch">
+                <div className="buttons">
+                  <button className="button is-danger">LEAVE CHAT</button>
                 </div>
-
-      return ( 
-        <div>
-        {main}
-              <LoadingScreen 
-          loading={loadingScreen} 
-          bgColor='#f1f1f1'
-          spinnerColor='#9ee5f8'
-          textColor='#676767'
-          logoSrc={TryImage}
-          text='Please wait, finding  Catches'
-          />
+              </Link>
+            </div>
+          </div>
         </div>
-      )
-    }
-  }
+        <Messages messages={messages} />
+        <form onSubmit={this.handleSubmit} id="form">
+          <div className="field has-addons">
+            <div className="control">
+              <input
+                type="text"
+                name="message"
+                className="input"
+                placeholder="Send message"
+                autoFocus
+                onChange={this.handleInput}
+              />
+            </div>
+            <div className="button is-info">
+              <button name="submit" className="login-button">
+                Send
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
 
+    return (
+      <div>
+        {main}
+        <LoadingScreen
+          loading={loadingScreen}
+          bgColor="#f1f1f1"
+          spinnerColor="#9ee5f8"
+          textColor="#676767"
+          logoSrc={TryImage}
+          text="Please wait, finding  Catches"
+        />
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => {
-  return { 
-    currChat: state.chat.currChat
-  }
-}
+  return {
+    currChat: state.chat.currChat,
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-  return { 
-    getChat: (chatId)=> dispatch(getChat(chatId))
-  }
-}
+  return {
+    getChat: chatId => dispatch(getChat(chatId)),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChatRoom);
--- src/client/store/reducers/chat.js
+++ src/client/store/reducers/chat.js
@@ -1,10 +1,11 @@
 /* eslint-disable default-case */
 // ACTION TYPES
-const CREATE_CHAT = 'CREATE_CHAT'
-const GET_CHAT = 'GET_CHAT'
+const CREATE_CHAT = 'CREATE_CHAT';
+const GET_CHAT = 'GET_CHAT';
+const ADD_MESSAGE = 'ADD_MESSAGE';
 
-export const createChat = ({ chatId, name, people, image, messages = []}) => {
-  console.log("TCL: createChat -> chatId,", chatId)
+export const createChat = ({ chatId, name, people, image, messages = [] }) => {
+  console.log('TCL: createChat -> chatId,', chatId);
   return {
     type: CREATE_CHAT,
     chat: {
@@ -12,38 +13,47 @@ export const createChat = ({ chatId, name, people, image, messages = []}) => {
       name,
       people,
       image,
-      messages
-    }
-  }
-}
+      messages,
+    },
+  };
+};
 
-export const getChat = (chatId) => {
+export const getChat = chatId => {
   return {
-type: GET_CHAT,
-chatId
-  }
-}
+    type: GET_CHAT,
+    chatId,
+  };
+};
+
+export const addMessage = message => {
+  return {
+    type: ADD_MESSAGE,
+    message,
+  };
+};
 
 const initialState = {
   chats: [],
-  currChat:{}
-}
+  currChat: [],
+};
 
 const chatReducer = (state = initialState, action) => {
-  switch(action.type){
+  switch (action.type) {
     case CREATE_CHAT:
-      return {...state, chats: [...state.chats, action.chat]}
-      case GET_CHAT:
-        let newChat = state.chats.filter((chat) => {
-          if(chat.chatId === action.chatId){
-            return chat
-          }
-        })
-        console.log('newChat => ', newChat)
-        return {...state, currChat:newChat[0]}
+      return { ...state, chats: [...state.chats, action.chat] };
+    case GET_CHAT:
+      let newChat = state.chats.filter(chat => {
+        if (chat.chatId === action.chatId) {
+          return chat;
+        }
+      });
+      console.log('newChat => ', newChat);
+      return { ...state, currChat: newChat[0] };
+    case ADD_MESSAGE:
+      return;
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default chatReducer
+export default chatReducer;
