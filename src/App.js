65e2c8f6ebe541bd309237a90c0d3c792194aa4d
Michael Mcmanus <mikemcmanus95@gmail.com>
2019-12-03 16:40:28 -0500

    fix: logout bug

--- src/App.js
+++ src/App.js
@@ -15,78 +15,84 @@ import { Route, Switch } from 'react-router-dom';
 import { verifyAuth } from './client/store/reducers/auth';
 
 class App extends Component {
-  componentDidMount() {
-    this.props.isVerified();
-  }
-
   render() {
     console.log('ARE WE LOGGED IN YET? ', this.props.isLoggedIn);
     console.log('ARE WE LOGGED IN YET WITH FIREBASE? ', this.props.fbLoggedIn);
-    const { isLoggedIn } = this.props;
+    const { isLoggedIn, isLoggedOut } = this.props;
     return (
       <div className="App">
-        <Switch>
-          <Route path="/signUp" component={SignUp} />
-          <Route exact path="/" component={SignIn} />
-          {isLoggedIn && (
-            <Switch>
-              <Route
-                path="/try"
-                render={() => (
-                  <div>
-                    <Try /> <Dashbar />
-                  </div>
-                )}
-              />{' '}
-              />
-              <Route
-                path="/await"
-                render={() => (
-                  <div>
-                    <AwaitList /> <Dashbar />
-                  </div>
-                )}
-              />{' '}
-              />
-              <Route
-                path="/catch"
-                render={() => (
-                  <div>
-                    <AllCatach /> <Dashbar />
-                  </div>
-                )}
-              />{' '}
-              />
-              <Route
-                path="/settings"
-                render={() => (
-                  <div>
-                    <Settings /> <Dashbar />
-                  </div>
-                )}
-              />{' '}
-              />
-              <Route
-                path="/splash"
-                render={() => (
-                  <div>
-                    <Splash /> <Dashbar />
-                  </div>
-                )}
-              />{' '}
-              />
-              <Route path="/chat/:id" component={ChatRoom} />
-            </Switch>
-          )}
-        </Switch>
+        {isLoggedOut ? (
+          <div>
+            <Route path="/signUp" component={SignUp} />
+            <Route exact path="/" component={SignIn} />
+          </div>
+        ) : (
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <div>
+                  <Try /> <Dashbar />
+                </div>
+              )}
+            />
+            <Route
+              path="/try"
+              render={() => (
+                <div>
+                  <Try /> <Dashbar />
+                </div>
+              )}
+            />{' '}
+            />
+            <Route
+              path="/await"
+              render={() => (
+                <div>
+                  <AwaitList /> <Dashbar />
+                </div>
+              )}
+            />{' '}
+            />
+            <Route
+              path="/catch"
+              render={() => (
+                <div>
+                  <AllCatach /> <Dashbar />
+                </div>
+              )}
+            />{' '}
+            />
+            <Route
+              path="/settings"
+              render={() => (
+                <div>
+                  <Settings /> <Dashbar />
+                </div>
+              )}
+            />{' '}
+            />
+            <Route
+              path="/splash"
+              render={() => (
+                <div>
+                  <Splash /> <Dashbar />
+                </div>
+              )}
+            />{' '}
+            />
+            <Route path="/chat/:id" component={ChatRoom} />
+          </Switch>
+        )}
       </div>
     );
   }
 }
 const mapState = state => {
   return {
-    isLoggedIn: state.auth.isAuthenticated,
-    fbLoggedIn: state.firebase.auth.isAuthenticated,
+    isLoggedIn: state.firebase.auth.isLoaded,
+    isLoggedOut: state.firebase.auth.isEmpty,
   };
 };
 
