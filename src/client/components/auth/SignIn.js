f3e97b6791c84e1cb76b8b2369c65f41bcf4856b
Michael Mcmanus <mikemcmanus95@gmail.com>
2019-12-03 11:18:44 -0500

    fix: fixed a bug that caused screens to scroll beyond content

--- src/client/components/auth/SignIn.js
+++ src/client/components/auth/SignIn.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { loginUser } from '../../store/reducers/auth';
 import { Redirect } from 'react-router-dom';
-import TryImage from './try.png'
-import { Link } from 'react-router-dom'
+import TryImage from './try.png';
+import { Link } from 'react-router-dom';
 
 class SignIn extends Component {
   constructor() {
@@ -32,64 +32,75 @@ class SignIn extends Component {
       return <Redirect to="/try" />;
     } else {
       return (
-        <section className="section" style={{
-        backgroundImage: `url(${TryImage})`,
-        backgroundRepeat:'no-repeat'
-      }}>
-          <form id="sign-in-form" onSubmit={this.handleSubmit} className="container">
-            {/* <div className="title" size="is-full">
+        <div id="signInBG">
+          <section
+            className="section"
+            style={{
+              backgroundImage: `url(${TryImage})`,
+              backgroundRepeat: 'no-repeat',
+            }}
+          >
+            <form
+              id="sign-in-form"
+              onSubmit={this.handleSubmit}
+              className="container"
+            >
+              {/* <div className="title" size="is-full">
               <h1>Sign In</h1>
             </div> */}
-            {/* EMAIL */}
-            <div className="field">
-              <label className="label has-text-white">Email</label>
-              <div className="control has-icons-left has-icons-right">
-                <input
-                  type="email"
-                  id="email"
-                  className="input"
-                  placeholder="Email input"
-                  onChange={this.handleChange}
-                />
-                <span className="icon is-small is-left">
-                  <icon className="fas fa-envelope"></icon>
-                </span>
-                <span className="icon is-small is-right">
-                  <icon className="fas fa-exclamation-triangle"></icon>
-                </span>
+              {/* EMAIL */}
+              <div className="field">
+                <label className="label has-text-white">Email</label>
+                <div className="control has-icons-left has-icons-right">
+                  <input
+                    type="email"
+                    id="email"
+                    className="input"
+                    placeholder="Email input"
+                    onChange={this.handleChange}
+                  />
+                  <span className="icon is-small is-left">
+                    <icon className="fas fa-envelope"></icon>
+                  </span>
+                  <span className="icon is-small is-right">
+                    <icon className="fas fa-exclamation-triangle"></icon>
+                  </span>
+                </div>
               </div>
-            </div>
-            {/* PASSWORD */}
-            <div className="field">
-              <label className="label has-text-white">Password</label>
-              <p className="control has-icons-left">
-                <input
-                  type="password"
-                  id="password"
-                  className="input"
-                  onChange={this.handleChange}
-                  placeholder="password"
-                />
-                <span className="icon is-small is-left">
-                  <icon className="fas fa-lock"></icon>
-                </span>
-              </p>
-              {loginError && (
-                <p className="error has-text-white">Incorrect email or password.</p>
-              )}
-            </div>
-            <div className="column" id='loggin'>
-              <button type="submit" className="button is-danger">
-                Login
-              </button>
-            </div>
-            <div id='signup-link'>
-              <Link to='/signup'>
-                  <h5 className="title is-5 has-text-white" >Sign Up</h5>
-              </Link>
-            </div>
-          </form>
-        </section>
+              {/* PASSWORD */}
+              <div className="field">
+                <label className="label has-text-white">Password</label>
+                <p className="control has-icons-left">
+                  <input
+                    type="password"
+                    id="password"
+                    className="input"
+                    onChange={this.handleChange}
+                    placeholder="password"
+                  />
+                  <span className="icon is-small is-left">
+                    <icon className="fas fa-lock"></icon>
+                  </span>
+                </p>
+                {loginError && (
+                  <p className="error has-text-white">
+                    Incorrect email or password.
+                  </p>
+                )}
+              </div>
+              <div className="column" id="loggin">
+                <button type="submit" className="button is-danger">
+                  Login
+                </button>
+              </div>
+              <div id="signup-link">
+                <Link to="/signup">
+                  <h5 className="title is-5 has-text-white">Sign Up</h5>
+                </Link>
+              </div>
+            </form>
+          </section>
+        </div>
       );
     }
   }
