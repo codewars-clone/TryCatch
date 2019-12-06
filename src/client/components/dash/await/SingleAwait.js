a657d52138da07510409187908e8c739cf5aab09
Sayeed Joseph <sayeedjoseph@gmail.com>
2019-12-06 13:58:40 -0500

    fix: change and add button color

--- src/client/components/dash/await/SingleAwait.js
+++ src/client/components/dash/await/SingleAwait.js
@@ -34,40 +34,56 @@ class SingleAwait extends Component {
               <img  src={imageUrl} alt="profile"/>
             </figure>
           </div>
-
           <div className="media-right">
             <div onClick={()=> {createChat(prospect)}}>
                 <div onClick={() => this.props.sendLike(userId)}>
-                <h4 className="title is-4" >{name}  <i className="far fa-heart"></i> </h4>
+                <h4 className="title is-4" >
+                  {name} 
+                    <button className='button is-small is-success'> 
+                      <i className="far fa-heart"></i> 
+                    </button>
+                </h4>
                 </div>
             </div>
           </div>
         </div>
-        <div className='wrapper'>
-        <button className="button is-success" onClick={()=> {this.handleModal()}}>See Code</button>
-        <div className={`modal ${active}`}>
-          <div className="modal-background"></div>
-          <div className="modal-card">
-            <header className="modal-card-head">
-              <h2 className="modal-card-title ">Coding Challenge</h2>
-              <button className="delete" aria-label="close"></button>
-            </header>
-            <section className="modal-card-body">
-            <pre>{message}</pre>
-            </section>
-            <footer className="modal-card-foot">
-              <button className="button is-danger" onClick={()=> {this.removeModal()}}>Cancel</button>
-            </footer>
-          </div>
-        </div>
-        <div class="media-right" onClick={() => this.props.unLike(userId)}>
-              <div className="buttons">
-                <button className="button is-small is-danger">
-                  <i className="fas fa-times"></i>
-                </button>
-              </div>
+        {message ? 
+          (<div className='wrapper'>
+            <button className="button is-link" onClick={()=> {this.handleModal()}}>See Code</button>
+          <div className={`modal ${active}`}>
+            <div className="modal-background"></div>
+            <div className="modal-card">
+              <header className="modal-card-head">
+                <h2 className="modal-card-title ">Coding Challenge</h2>
+                <button className="delete" aria-label="close"></button>
+              </header>
+              <section className="modal-card-body">
+              <pre>{message}</pre>
+              </section>
+              <footer className="modal-card-foot">
+                <button className="button is-danger" onClick={()=> {this.removeModal()}}>Back</button>
+              </footer>
             </div>
-        </div> 
+          </div>
+          <div class="media-right" onClick={() => this.props.unLike(userId)}>
+                <div className="buttons">
+                  <button className="button is-small is-danger">
+                    <i className="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+          </div>
+        ): 
+        (<div className='wrapper'>
+          <div></div>
+          <div class="media-right" onClick={() => this.props.unLike(userId)}>
+                <div className="buttons">
+                  <button className="button is-small is-danger">
+                    <i className="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+          </div>)}
       </div>
     );
   }
