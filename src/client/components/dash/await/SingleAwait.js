adb70f89f8e48ea3a3dd8a1eabb6a334a8a4d7e3
Sayeed Joseph <sayeedjoseph@gmail.com>
2019-12-06 13:46:20 -0500

    feat: add coding challenge view

--- src/client/components/dash/await/SingleAwait.js
+++ src/client/components/dash/await/SingleAwait.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
-import { LikeButton } from '../../index'
-import  NextButton from '../NextButton';
 class SingleAwait extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { 
+      active: ''
+    }
+    this.handleModal = this.handleModal.bind(this)
+    this.removeModal = this.removeModal.bind(this)
   }
-  render() {
-    const { name, age, imageUrl, userId } = this.props.prospect
-    const { createChat, prospect } = this.props
 
+  handleModal(){
+    this.setState({
+      active: 'is-active'
+    })
+  }
+
+  removeModal(){
+    this.setState({
+      active: ''
+    })
+  }
+
+  render() {
+    const { name, imageUrl, userId, message } = this.props.prospect
+    console.log("TCL: SingleAwait -> render ->  message",  message)
+    const { createChat, prospect } = this.props
+    const { active } = this.state
     return (
       <div className="box">
         <div className="media">
@@ -17,23 +33,41 @@ class SingleAwait extends Component {
             <figure className="image is-64x64">
               <img  src={imageUrl} alt="profile"/>
             </figure>
-            <div class="media-left" onClick={() => this.props.unLike(userId)}>
-              <div className="buttons">
-                <button className="button is-danger">
-                  <i className="fas fa-times"></i>
-                </button>
-              </div>
-            </div>
           </div>
+
           <div className="media-right">
-            <h4 className="title is-4" >{name} {age} <i className="fas fa-birthday-cake"></i></h4>
             <div onClick={()=> {createChat(prospect)}}>
                 <div onClick={() => this.props.sendLike(userId)}>
-                <LikeButton />
+                <h4 className="title is-4" >{name}  <i className="far fa-heart"></i> </h4>
                 </div>
             </div>
           </div>
         </div>
+        <div className='wrapper'>
+        <button className="button is-success" onClick={()=> {this.handleModal()}}>See Code</button>
+        <div className={`modal ${active}`}>
+          <div className="modal-background"></div>
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <h2 className="modal-card-title ">Coding Challenge</h2>
+              <button className="delete" aria-label="close"></button>
+            </header>
+            <section className="modal-card-body">
+            <pre>{message}</pre>
+            </section>
+            <footer className="modal-card-foot">
+              <button className="button is-danger" onClick={()=> {this.removeModal()}}>Cancel</button>
+            </footer>
+          </div>
+        </div>
+        <div class="media-right" onClick={() => this.props.unLike(userId)}>
+              <div className="buttons">
+                <button className="button is-small is-danger">
+                  <i className="fas fa-times"></i>
+                </button>
+              </div>
+            </div>
+        </div> 
       </div>
     );
   }
